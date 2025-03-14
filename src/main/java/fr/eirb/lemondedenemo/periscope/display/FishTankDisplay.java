229c379e3c4f536139115dd1d5cd6784b0fe47eb
Martin Eyben <martin.eyben@enseirb-matmeca.fr>
2025-03-14 12:21:39 +0100

    feat[display]: add fish tank display interface

---
+++ src/main/java/fr/eirb/lemondedenemo/periscope/display/FishTankDisplay.java
@@ -0,0 +1,38 @@
+package fr.eirb.lemondedenemo.periscope.display;
+
+import fr.eirb.lemondedenemo.periscope.utils.Coords;
+import fr.eirb.lemondedenemo.periscope.utils.Fish;
+
+public interface FishTankDisplay {
+
+  /** Start the display */
+  void start();
+
+  /** Stop the display */
+  void stop();
+
+  /**
+   * Add a fish in the tank
+   *
+   * @param id fish id
+   * @param fish fish representation
+   * @param coords where to place the fish in the tank
+   */
+  void addFish(String id, Fish fish, Coords coords);
+
+  /**
+   * Remove a fish from the tank
+   *
+   * @param id fish id
+   */
+  void removeFish(String id);
+
+  /**
+   * Move a fish in the tank
+   *
+   * @param id fish id
+   * @param coords where the fish goes
+   * @param duration duration of the transition
+   */
+  void moveFish(String id, Coords coords, int duration);
+}
---
+++ src/main/java/fr/eirb/lemondedenemo/periscope/utils/Fish.java
@@ -0,0 +1,11 @@
+package fr.eirb.lemondedenemo.periscope.utils;
+
+public class Fish {
+  public int length;
+  public int height;
+
+  public Fish(int length, int height) {
+    this.length = length;
+    this.height = height;
+  }
+}
