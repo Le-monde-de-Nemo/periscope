03cb3d8cea3f10c43c90577251d9ba9c16e646ca
Alessevan <44125445+Alessevan@users.noreply.github.com>
2025-03-15 21:03:58 +0100

    feat: use record for Coords

--- src/main/java/fr/eirb/lemondedenemo/periscope/utils/Coords.java
+++ src/main/java/fr/eirb/lemondedenemo/periscope/utils/Coords.java
@@ -1,11 +1,29 @@
 package fr.eirb.lemondedenemo.periscope.utils;
 
-public class Coords {
-  public int x;
-  public int y;
+public record Coords(int x, int y) {
 
   public Coords(int x, int y) {
     this.x = x;
     this.y = y;
+  }
+
+  public Coords add(Coords other) {
+    return new Coords(x + other.x, y + other.y);
+  }
+
+  public Coords sub(Coords other) {
+    return new Coords(x - other.x, y - other.y);
+  }
+
+  public Coords mul(int factor) {
+    return new Coords(x * factor, y * factor);
+  }
+
+  public Coords div(int factor) {
+    return new Coords(x / factor, y / factor);
+  }
+
+  public int distance(Coords other) {
+    return Math.abs(x - other.x) + Math.abs(y - other.y);
   }
 }
