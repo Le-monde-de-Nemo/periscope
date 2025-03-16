e7ec1ba89cb7e4b717e5f07edcf16c36d54d88fa
Alessevan <44125445+Alessevan@users.noreply.github.com>
2025-03-16 22:25:55 +0100

    feat: bare minimum network handling

---
+++ src/main/java/fr/eirb/lemondedenemo/periscope/network/FishConnection.java
@@ -0,0 +1,121 @@
+package fr.eirb.lemondedenemo.periscope.network;
+
+import fr.eirb.lemondedenemo.periscope.api.events.ConnectionReady;
+import fr.eirb.lemondedenemo.periscope.api.events.HandShakeReceiveEvent;
+import fr.eirb.lemondedenemo.periscope.api.events.PongReceiveEvent;
+import fr.eirb.lemondedenemo.periscope.api.network.Connection;
+import fr.eirb.lemondedenemo.periscope.api.network.packets.Packet;
+import fr.eirb.lemondedenemo.periscope.events.FishEventManager;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+import java.util.Locale;
+import java.util.Optional;
+import org.apache.logging.log4j.Logger;
+
+public class FishConnection implements Connection {
+
+  private static final int TIMEOUT = 5000;
+
+  private final Logger logger;
+  private final FishEventManager events;
+  private final InetSocketAddress address;
+
+  private final Socket socket;
+  private PrintWriter writer;
+  private Thread reader;
+
+  public FishConnection(Logger logger, String address, int port, FishEventManager events) {
+    this.logger = logger;
+    this.events = events;
+    this.address = new InetSocketAddress(address, port);
+    this.socket = new Socket();
+  }
+
+  @Override
+  public void connect() throws IOException {
+    this.socket.connect(this.address, TIMEOUT);
+    this.logger.info(
+        "Connected to server at "
+            + this.address.getHostName()
+            + ":"
+            + this.address.getPort()
+            + ".");
+    this.events.fireEvent(new ConnectionReady(this.address));
+
+    // Create in out streams
+    this.writer = new PrintWriter(this.socket.getOutputStream(), false);
+    this.reader =
+        new Thread(
+            () -> {
+              try {
+                byte[] buffer = new byte[1024];
+                int read;
+                while ((read = this.socket.getInputStream().read(buffer)) != -1) {
+                  this.receive(new String(buffer, 0, read));
+                }
+              } catch (IOException e) {
+                this.logger.error(e);
+              }
+            });
+    this.reader.setName("ConnectionReader");
+    this.reader.start();
+
+    Runtime.getRuntime()
+        .addShutdownHook(
+            new Thread(
+                () -> {
+                  try {
+                    this.disconnect();
+                  } catch (IOException e) {
+                    this.logger.error("Cannot close connection.", e);
+                  }
+                }));
+  }
+
+  public boolean isConnected() {
+    return this.socket.isConnected();
+  }
+
+  @Override
+  public void disconnect() throws IOException {
+    if (!this.socket.isConnected()) {
+      return;
+    }
+    if (!this.reader.isInterrupted()) this.reader.interrupt();
+    this.writer.close();
+    this.socket.close();
+    this.logger.info("Disconnected from server.");
+  }
+
+  private void receive(String message) {
+    if (message.isEmpty()) {
+      return;
+    }
+    this.logger.debug("Received message: " + message);
+    String[] components = message.split(" ");
+    switch (components[0].toLowerCase(Locale.ROOT)) {
+      case "no", "greeting" ->
+          this.events.fireEvent(
+              new HandShakeReceiveEvent(
+                  Optional.ofNullable(
+                      components[0].equalsIgnoreCase("greeting") ? components[1] : null)));
+      case "pong" -> {
+        try {
+          this.events.fireEvent(new PongReceiveEvent(Integer.parseInt(components[1])));
+        } catch (NumberFormatException e) {
+          this.logger.error(
+              "Invalid pong packet. Server may be corrupted, exiting to prevent next errors.", e);
+          System.exit(1);
+        }
+      }
+    }
+  }
+
+  @Override
+  public void send(Packet packet) {
+    this.writer.println(packet.serialize());
+    this.writer.flush();
+  }
+}
