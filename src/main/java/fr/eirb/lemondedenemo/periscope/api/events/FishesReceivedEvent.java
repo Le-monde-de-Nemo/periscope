ff3e808d57d31702bddc1fb97317abae3c83ac22
Martin Eyben <martin.eyben@enseirb-matmeca.fr>
2025-04-18 16:38:20 +0200

    fix[FishReceived]: format to string

--- src/main/java/fr/eirb/lemondedenemo/periscope/api/events/FishesReceivedEvent.java
+++ src/main/java/fr/eirb/lemondedenemo/periscope/api/events/FishesReceivedEvent.java
@@ -11,7 +11,7 @@ public record FishesReceivedEvent(List<FishDestination> fishes) implements Event
     @Override
     public String toString() {
       return String.format(
-          "New fish : %s; destination : x: %.0f y: %.0f; size: %.0fx%.0f; duration: %.0f",
+          "Fish destination: %s; destination : x: %.0f y: %.0f; size: %.0fx%.0f; duration: %.0f",
           fish.getName(),
           destination.x(),
           destination.y(),
