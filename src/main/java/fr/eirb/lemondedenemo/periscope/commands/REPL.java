811363b3ac4d7f31ec6422c2ad8c4c09ef020129
Martin Eyben <martin.eyben@enseirb-matmeca.fr>
2025-04-19 12:45:49 +0200

    feat[REPL]: upgrade to new version of JLine

--- src/main/java/fr/eirb/lemondedenemo/periscope/commands/REPL.java
+++ src/main/java/fr/eirb/lemondedenemo/periscope/commands/REPL.java
@@ -5,18 +5,26 @@ import fr.eirb.lemondedenemo.periscope.api.commands.manager.CommandManager.Comma
 import fr.eirb.lemondedenemo.periscope.api.commands.manager.CommandResult;
 import java.io.*;
 import java.nio.charset.StandardCharsets;
+import java.util.concurrent.ExecutionException;
 import java.util.regex.Matcher;
-import jline.console.ConsoleReader;
 import org.apache.logging.log4j.Logger;
+import org.jline.reader.EndOfFileException;
+import org.jline.reader.LineReader;
+import org.jline.reader.LineReaderBuilder;
+import org.jline.reader.impl.completer.StringsCompleter;
+import org.jline.terminal.Terminal;
+import org.jline.terminal.TerminalBuilder;
 
 public final class REPL extends Thread {
 
-  private static final String INPUT_REPL = "\33[31m> \33[0m";
-  private static final String OUTPUT_REPL = "\33[31m< \33[0m";
+  private static final String INPUT_REPL = "\33[31m> \033[0m";
+  private static final String OUTPUT_REPL = "\33[31m< \033[0m";
   private final Logger logger;
   private final CommandManager commands;
   private final InputStream in;
   private final OutputStream out;
+  private LineReader reader;
+  private Terminal terminal;
 
   public REPL(Logger logger, CommandManager commands, InputStream in, OutputStream out) {
     this.logger = logger;
@@ -28,35 +36,56 @@ public final class REPL extends Thread {
     this.setName("REPL");
   }
 
-  @Override
-  public void run() {
+  private void exitREPL() {
+    // Exit
     try {
-      ConsoleReader reader = new ConsoleReader(this.in, this.out);
-      String line;
-      while ((line = reader.readLine(INPUT_REPL)) != null) {
-        if (line.isEmpty()) continue;
-
-        logger.info("Console: {}", line);
-        Matcher matcher = null;
-        for (Command command : Command.values()) {
-          matcher = command.getPattern().matcher(line);
-          if (!matcher.find()) continue;
-
-          CommandResult result = this.commands.execute(command, matcher).get();
-          out.write((OUTPUT_REPL + result.getMessage() + "\n").getBytes(StandardCharsets.UTF_8));
-          break;
-        }
-
-        assert matcher != null;
-        matcher.reset();
-        if (!matcher.find())
-          out.write((OUTPUT_REPL + "NOK : command inconnue.\n").getBytes(StandardCharsets.UTF_8));
-      }
-
-      // Exit
       CommandResult result =
           this.commands.execute(Command.EXIT, Command.EXIT.getPattern().matcher("bye")).get();
       out.write((OUTPUT_REPL + result.getMessage() + "\n").getBytes(StandardCharsets.UTF_8));
+    } catch (Exception e) {
+      logger.error(e);
+    }
+  }
+
+  private void setupREPL() throws IOException {
+    this.terminal = TerminalBuilder.builder().system(false).streams(this.in, this.out).build();
+    this.reader = LineReaderBuilder.builder().terminal(terminal).build();
+  }
+
+  private void readREPL() throws ExecutionException, InterruptedException {
+    String line;
+    while ((line = reader.readLine(INPUT_REPL)) != null) {
+      if (line.isEmpty()) continue;
+
+      logger.trace("Console: {}", line);
+      Matcher matcher = null;
+      for (Command command : Command.values()) {
+        matcher = command.getPattern().matcher(line);
+        if (!matcher.find()) continue;
+
+        CommandResult result = this.commands.execute(command, matcher).get();
+        terminal.writer().println(OUTPUT_REPL + result.getMessage());
+        break;
+      }
+
+      assert matcher != null;
+      matcher.reset();
+      if (!matcher.find()) terminal.writer().println(OUTPUT_REPL + "NOK: unknown command");
+    }
+  }
+
+  @Override
+  public void run() {
+    try {
+
+      setupREPL();
+
+      readREPL();
+
+      exitREPL();
+
+    } catch (EndOfFileException e) {
+      exitREPL();
     } catch (Exception e) {
       logger.error(e);
     }
